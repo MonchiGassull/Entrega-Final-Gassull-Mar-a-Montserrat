e916f457e11c4117f533cb2699fbbae6ff9bfd2d
María Montserrat Gassull <montserratgassull@gmail.com>
2023-05-25 00:32:55 -0300

    Agregados modelos Escritor, Lector y Artículo en la app BlogCocina

--- BlogCocina/models.py
+++ BlogCocina/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Escritor(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+
+class Lector(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    
+class Artículo(models.Model):
+    titulo = models.CharField(max_length=256)
+    subtitulo = models.TextField()
+    desarrollo = models.TextField()
+    autor = models.CharField(max_length=256)
+    fecha_publicacion = models.DateField()
