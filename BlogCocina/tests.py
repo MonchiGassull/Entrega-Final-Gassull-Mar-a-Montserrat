ceba463ac1b7c9e03c12737c4a4b0a13c035f899
María Montserrat Gassull <montserratgassull@gmail.com>
2023-05-28 19:59:03 -0300

    Agrgados test unitarios

--- BlogCocina/tests.py
+++ BlogCocina/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from BlogCocina.models import Articulo
 
 # Create your tests here.
+
+class ArticuloTests(TestCase):
+    def test_creacion_articulo(self):
+        articulo = Articulo(titulo = "Titulo", subtitulo = "Subtitulo", cuerpo = "Cuerpo", autor = "Autor")
+        articulo.save()
+        
+        self.assertEqual(Articulo.objects.count(), 1)
+        self.assertIsNotNone(articulo.id)
+        
+    def test_articulo_str(self):
+        articulo = Articulo(titulo = "Titulo", subtitulo = "Subtitulo", cuerpo = "Cuerpo", autor = "Autor")
+        articulo.save()
+
+        self.assertEqual(articulo._str_(), "Titulo | Subtitulo | Autor")
